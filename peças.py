5ed4a28211dbc1a20bdea468285affa14bf55eee
Faaler <depaularafaelvictor@gmail.com>
2024-11-05 10:39:17 -0300

    craindo pecas

--- peças.py
+++ peças.py
@@ -1,3 +1,19 @@
-class Pecas:
-    def __init__(self):
-        pass
+import numpy as np
+
+class Peca(np.ndarray):
+    def __new__(cls, input_array):
+        obj = np.asarray(input_array).view(cls)
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None: return
+        self.info = "Esta é uma peça do jogo"
+
+    
+
+peca1 = Peca([
+        [0, 0, 0],
+        [0, 0, 0]
+    ])
+
+print(peca1)
