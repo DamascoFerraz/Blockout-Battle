2bfca83137e4194ccdefc7949ab044470d737608
Faaler <depaularafaelvictor@gmail.com>
2024-11-12 09:23:34 -0300

    pera um poco

---
+++ tkinter_blocks.py
@@ -0,0 +1,36 @@
+from tkinter import *
+from importar import *
+import tkinter.messagebox
+root = Tk()
+root.title('BLOCKOUT_BATTLE')
+root.geometry('350x200')
+lbl = Label(root, text='BLOCKOUT_BATTLE')
+lbl.grid()
+
+txt_resultado = root.Text(root, height=2, width=16, font=('Arial', 24))
+txt_resultado.grid(columnspan=5)
+
+
+
+
+
+#-------------------------------------------------------
+os.system("cls")
+
+run = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
